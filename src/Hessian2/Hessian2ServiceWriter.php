d6f0fcba10d9badb83cb96187447fd867dc39ab8
vegeta.ec <vegeta.ec@gmail.com>
2011-07-09 06:07:25 +0000

    Small change in headers and fix to issue 15

--- src/Hessian2/Hessian2ServiceWriter.php
+++ src/Hessian2/Hessian2ServiceWriter.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is part of the HessianPHP package.
- * (c) 2004-2010 Manuel Gómez
+ * (c) 2004-2011 Manuel Gómez
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
